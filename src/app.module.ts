0942c0ff306a6f01cd8c901a2039d39cd59851bc
oybekalloyorov <alloyorovoybek1997@gmail.com>
2026-03-09 13:11:21 +0500

    viewer.html log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerService } from './logs/logger.service';
+import { LogGateway } from './logs/log.gateway';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ActivityInterceptor } from './logs/activity.interceptor';
+import { LogController } from './logs/log.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, LogController],
+  providers: [AppService,
+    LoggerService,
+    LogGateway,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ActivityInterceptor
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
